56d991723b184b4f8ab10e055fb33d658b413ed9
anshsangal12 <sangalansh4561@gmail.com>
2026-03-28 22:49:46 +0530

    Added SSL config for Render database connection

--- db.js
+++ db.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    // This allows the server to connect to Render's hosted database securely
+    rejectUnauthorized: false 
+  }
 });
 
 module.exports = pool;
